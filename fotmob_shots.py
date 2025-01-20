047b45e4cf2efa95a2ada2c8c2fa180cf8737dc2
axelbol <axel_lp_alvarado@hotmail.com>
2025-01-20 12:07:02 -0400

    added general info & color if statement

--- fotmob_shots.py
+++ fotmob_shots.py
@@ -4,6 +4,10 @@ from bs4 import BeautifulSoup as bs
 import pandas as pd
 import matplotlib.pyplot as plt
 from mplsoccer.pitch import Pitch
+import matplotlib.font_manager as font_manager
+font_path = '/home/axel/Code/Python/axel/files/fonts/Arvo/Arvo-Regular.ttf'
+# Create a font properties object with the font file
+font_props = font_manager.FontProperties(fname=font_path)
 
 def fetch_match_data(url):
     """Fetch and parse match data from FotMob."""
@@ -23,10 +27,29 @@ def process_shots_data(json_fotmob):
     away_team_name = json_fotmob['props']['pageProps']['header']['teams'][1]['name']
     local_team_score = json_fotmob['props']['pageProps']['header']['teams'][0]['score']
     away_team_score = json_fotmob['props']['pageProps']['header']['teams'][1]['score']
+    # ball possesion
+    local_ball_possesion = json_fotmob['props']['pageProps']['content']['stats']['Periods']['All']['stats'][0]['stats'][0]['stats'][0]
+    away_ball_possesion = json_fotmob['props']['pageProps']['content']['stats']['Periods']['All']['stats'][0]['stats'][0]['stats'][1]
+    # xG
+    local_xG = json_fotmob['props']['pageProps']['content']['stats']['Periods']['All']['stats'][0]['stats'][1]['stats'][0]
+    away_xG = json_fotmob['props']['pageProps']['content']['stats']['Periods']['All']['stats'][0]['stats'][1]['stats'][1]
+    # total shots
+    local_total_shots = json_fotmob['props']['pageProps']['content']['stats']['Periods']['All']['stats'][0]['stats'][2]['stats'][0]
+    away_total_shots = json_fotmob['props']['pageProps']['content']['stats']['Periods']['All']['stats'][0]['stats'][2]['stats'][1]
+    # shots on target
+    local_shots_target = json_fotmob['props']['pageProps']['content']['stats']['Periods']['All']['stats'][0]['stats'][3]['stats'][0]
+    away_shots_target = json_fotmob['props']['pageProps']['content']['stats']['Periods']['All']['stats'][0]['stats'][3]['stats'][1]
+    # team colors
+    home_color = json_fotmob['props']['pageProps']['general']['teamColors']['darkMode']['home']
+    if home_color == '#ffffff':
+        home_color = '#444444'
+    away_color = json_fotmob['props']['pageProps']['general']['teamColors']['darkMode']['away']
+    if away_color == '#ffffff':
+        away_color = '#444444'
 
-    return df_shots, local_team_name, away_team_name, local_team_score, away_team_score, leagueName, leagueRound, local_team_id
+    return df_shots, local_team_name, away_team_name, local_team_score, away_team_score, local_ball_possesion, away_ball_possesion, local_xG, away_xG, local_total_shots, away_total_shots, local_shots_target, away_shots_target, home_color, away_color, leagueName, leagueRound, local_team_id
 
-def plot_shots(df_shots, local_team_name, away_team_name, local_team_score, away_team_score, league_title, local_team_id):
+def plot_shots(df_shots, local_team_name, away_team_name, local_team_score, away_team_score, local_ball_possesion, away_ball_possesion, local_xG, away_xG, local_total_shots, away_total_shots, local_shots_target, away_shots_target, home_color, away_color, league_title, local_team_id):
     """Plot the shots on a soccer pitch."""
     plot_title = f"{local_team_name} {local_team_score}-{away_team_score} {away_team_name}"
     shots_local_name = f"{local_team_name.upper()} SHOTS"
@@ -126,6 +149,28 @@ def plot_shots(df_shots, local_team_name, away_team_name, local_team_score, away
 
     # Add info and save the plot
     ax.text(60, -2, 'Data via Opta | @axel_bol', ha='left', fontsize=8, fontfamily='monospace')
+
+    # General INFO
+    ax.text(x=52.5, y=50, s='GOALS', ha='center', fontsize=25, fontproperties=font_props,fontweight=800, color='black')
+    ax.text(x=39.5, y=50, s=local_team_score, ha='center', fontsize=25, fontproperties=font_props,fontweight=800, color=home_color)
+    ax.text(x=65.5, y=50, s=away_team_score, ha='center', fontsize=25, fontproperties=font_props,fontweight=800, color=away_color)
+
+    ax.text(x=52.5, y=44, s='xG', ha='center', fontsize=25, fontproperties=font_props,fontweight=800, color='black')
+    ax.text(x=39.5, y=44, s=local_xG, ha='center', fontsize=25, fontproperties=font_props,fontweight=800, color=home_color)
+    ax.text(x=65.5, y=44, s=away_xG, ha='center', fontsize=25, fontproperties=font_props,fontweight=800, color=away_color)
+
+    ax.text(x=52.5, y=36, s='SHOTS', ha='center', fontsize=25, fontproperties=font_props,fontweight=800, color='black')
+    ax.text(x=39.5, y=36, s=local_total_shots, ha='center', fontsize=25, fontproperties=font_props,fontweight=800, color=home_color)
+    ax.text(x=65.5, y=36, s=away_total_shots, ha='center', fontsize=25, fontproperties=font_props,fontweight=800, color=away_color)
+
+    ax.text(x=52.5, y=28, s='ON TARGET', ha='center', fontsize=25, fontproperties=font_props,fontweight=800, color='black')
+    ax.text(x=39.5, y=28, s=local_shots_target, ha='center', fontsize=25, fontproperties=font_props,fontweight=800, color=home_color)
+    ax.text(x=65.5, y=28, s=away_shots_target, ha='center', fontsize=25, fontproperties=font_props,fontweight=800, color=away_color)
+
+    ax.text(x=52.5, y=20, s='POSESSION', ha='center', fontsize=25, fontproperties=font_props,fontweight=800, color='black')
+    ax.text(x=39.5, y=20, s=f"{local_ball_possesion}%", ha='center', fontsize=25, fontproperties=font_props,fontweight=800, color=home_color)
+    ax.text(x=65.5, y=20, s=f"{away_ball_possesion}%", ha='center', fontsize=25, fontproperties=font_props,fontweight=800, color=away_color)
+
     plt.savefig('/home/axel/Code/images/image_script.png', dpi=fig.dpi, bbox_inches='tight', pad_inches=0.35)
 
     # plt.show()
@@ -135,11 +180,12 @@ def main():
     json_fotmob = fetch_match_data(url_input)
 
     # Extract relevant data
-    df_shots, local_team_name, away_team_name, local_team_score, away_team_score, leagueName, leagueRound, local_team_id = process_shots_data(json_fotmob)
+    df_shots, local_team_name, away_team_name, local_team_score, away_team_score, local_ball_possesion, away_ball_possesion, local_xG, away_xG, local_total_shots, away_total_shots, local_shots_target, away_shots_target, home_color, away_color, leagueName, leagueRound, local_team_id = process_shots_data(json_fotmob)
+
     league_title = leagueName + ' | ' + leagueRound
 
     # Plot the data
-    plot_shots(df_shots, local_team_name, away_team_name, local_team_score, away_team_score, league_title, local_team_id)
+    plot_shots(df_shots, local_team_name, away_team_name, local_team_score, away_team_score, local_ball_possesion, away_ball_possesion, local_xG, away_xG, local_total_shots, away_total_shots, local_shots_target, away_shots_target, home_color, away_color, league_title, local_team_id)
 
     print('Done!')
 
